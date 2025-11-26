d1203a4eaf54c28a00977f89d243c5c578c29c53
mcaserto <mscaserto@gmail.com>
2025-11-25 22:50:41 -0500

    initial commit with nix files

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Embedded Rust development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "thumbv6m-none-eabi" ]; # arm cortex-M0
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            toolchain
+            rust-analyzer
+            rustfmt
+            clippy
+            gdb # Or other debugger
+            openocd # For flashing/debugging
+          ];
+
+          RUST_TARGET = "thumbv6m-none-eabi";
+      };
+      });
+}
